9bb4ec5390c64ebc185bca8ee8bdb9c4db50ff25
Haamed Gheibi <gheibi@gmail.com>
2026-02-26 03:05:28 -0800

    init

--- apps/latif-api/src/app/app.module.ts
+++ apps/latif-api/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
